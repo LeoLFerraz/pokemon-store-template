33facc4b25bee36eeb0971d3f25b7a2ab6cad201
Leonardo Ferraz <ldlferraz@gmail.com>
2020-07-30 14:40:18 -0300

    Adding some style and content to header.

--- src/viewparts/Header.js
+++ src/viewparts/Header.js
@@ -2,6 +2,8 @@ import React from 'react';
 import "../assets/styles/viewparts/Header.scss";
 import { DefaultCarousel } from "../components/DefaultCarousel";
 import { InfoCircle, ExclamationCircle } from "react-bootstrap-icons";
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from "react-bootstrap";
+import { ReactComponent as PokeStoreLogo } from "../assets/svg/Pokeball.svg"
 
 export function Header(props) {
     // Tipbar (hidden if scroll-top is anywhere but 0);
@@ -17,11 +19,31 @@ export function Header(props) {
                     </DefaultCarousel>
                 </div>
                 <div className="container">
-                    <div id="header">
-                        Header
-                    </div>
-                    <nav>
-                        Menu
+                    <nav id="main-nav">
+                        <Navbar expand="lg">
+                            <Navbar.Brand>
+                                <PokeStoreLogo id="pokestoreLogo" />
+                                <div className="brand-name">PokeStore<br/>Fire</div>
+                            </Navbar.Brand>
+                            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                            <Navbar.Collapse id="basic-navbar-nav">
+                                <Nav className="mr-auto">
+                                    <NavDropdown title="Departments" className="departments-dropdown">
+                                        <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                        <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                        <NavDropdown.Divider />
+                                        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                    </NavDropdown>
+                                    <Nav.Link href="#home">Home</Nav.Link>
+                                    <Nav.Link href="#link">Link</Nav.Link>
+                                </Nav>
+                                <Form inline>
+                                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                                    <Button variant="outline-success">Search</Button>
+                                </Form>
+                            </Navbar.Collapse>
+                        </Navbar>
                     </nav>
                 </div>
             </header>
