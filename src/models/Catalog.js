98d5b73a6d7c70c828824cd5cc34208c984ae198
Leonardo Ferraz <ldlferraz@gmail.com>
2020-07-31 13:51:09 -0300

    Adding catalog model scaffolding. Starting catalog page.

--- src/models/Catalog.js
+++ src/models/Catalog.js
@@ -1,9 +1,24 @@
+import store from "../redux/store";
+
 class Catalog {
+    /**
+     *
+     * @param filters - Informs which attributes should be filtered in the collection. You can pass either raw values
+     * for 'equals' comparisons (id: 5) or an object as below:
+     * {
+     *     id: {
+     *         gt: 5
+     *         lt: 10
+     *     }
+     * }
+     * Filterable attributes: id, name, generation, types
+     * @param collection - Informs which collection this is being filtered from. Usually gotten from redux store.
+     */
     constructor(filters, collection) {
 
+        console.log(filters);
+        console.log(collection);
     }
 }
-
-
 
 export default Catalog;
--- src/templates/Catalog.js
+++ src/templates/Catalog.js
@@ -1,6 +1,12 @@
 import React from 'react'
+import CatalogModel from "../models/Catalog";
+import { useSelector } from 'react-redux';
 
 export function Catalog(props) {
+    let pokeData = useSelector((store) => {return store.pokemonData.pokemon});
+
+    let cat = new CatalogModel({id: 5}, pokeData);
+    let cat2 = new CatalogModel({id: {gt: 5}}, pokeData);
 
     return (
         <main>
